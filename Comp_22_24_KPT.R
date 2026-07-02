84d6da67e6d376c7eec742a1d803bccc02315cc6
Haász Evelin <haaszevelin@gmail.com>
2026-07-02 12:57:37 +0200

    invariance testing

--- Comp_22_24_KPT.R
+++ Comp_22_24_KPT.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(dplyr)
 library(knitr)
+library(lavaan)
 
 KPT_uagyerekek22_24_comp <- read_excel("C:/Users/Haász Evelin/Desktop/Egyetem/Kognitív Képességek Kutatócsoport/Connections between sensorimotor and cognitive abilities, and school performance/KPT2/KPT_uagyerekek22_24_comp.xlsx")
 View(KPT_uagyerekek22_24_comp)
@@ -71,5 +72,243 @@ table_fit[2, ] <- c("Overall Model2 ", round(fitmeasures(overall.fit2,
 kable(table_fit)
 
 #------------------------
-#longitudinal CFA
+#longitudinal MGCFA
 #------------------------
+# ============================================================
+# LONGITUDINAL CFA + MEASUREMENT INVARIANCE
+# ============================================================
+
+library(semTools)
+
+# ============================================================
+# 1. LONGITUDINAL CONFIGURAL MODEL
+# ============================================================
+
+long_model_configural <- '
+
+# ------------------------------------------------
+# TIME 1 (2022)
+# ------------------------------------------------
+
+Gf_1 =~ Raven_1 + Figurak_1
+
+GWM_1 =~ Corsi_1 +
+         Szamismetles_1 +
+         Szamsorozatvisszafele_1
+
+PSMF_1 =~ Ujjak_1 + Kezek_1
+
+BOF_1 =~ Egyensuly_nyitottszem_jobb_1 +
+         Egyensuly_nyitottszem_bal_1
+
+BCF_1 =~ Egyensuly_csukottszem_jobb_1 +
+         Egyensuly_csukottszem_bal_1
+
+
+# ------------------------------------------------
+# TIME 2 (2024)
+# ------------------------------------------------
+
+Gf_2 =~ Raven_2 + Figurak_2
+
+GWM_2 =~ Corsi_2 +
+         Szamismetles_2 +
+         Szamsorozatvisszafele_2
+
+PSMF_2 =~ Ujjak_2 + Kezek_2
+
+BOF_2 =~ Egyensuly_nyitottszem_jobb_2 +
+         Egyensuly_nyitottszem_bal_2
+
+BCF_2 =~ Egyensuly_csukottszem_jobb_2 +
+         Egyensuly_csukottszem_bal_2
+
+
+# ------------------------------------------------
+# LATENT STABILITY
+# ------------------------------------------------
+
+Gf_1 ~~ Gf_2
+GWM_1 ~~ GWM_2
+PSMF_1 ~~ PSMF_2
+BOF_1 ~~ BOF_2
+BCF_1 ~~ BCF_2
+
+
+# ------------------------------------------------
+# WITHIN-TIME FACTOR CORRELATIONS
+# ------------------------------------------------
+
+Gf_1 ~~ GWM_1 + PSMF_1 + BOF_1 + BCF_1
+GWM_1 ~~ PSMF_1 + BOF_1 + BCF_1
+PSMF_1 ~~ BOF_1 + BCF_1
+BOF_1 ~~ BCF_1
+
+Gf_2 ~~ GWM_2 + PSMF_2 + BOF_2 + BCF_2
+GWM_2 ~~ PSMF_2 + BOF_2 + BCF_2
+PSMF_2 ~~ BOF_2 + BCF_2
+BOF_2 ~~ BCF_2
+
+
+# ------------------------------------------------
+# CORRELATED UNIQUENESSES
+# ------------------------------------------------
+
+Raven_1 ~~ Raven_2
+Figurak_1 ~~ Figurak_2
+
+Corsi_1 ~~ Corsi_2
+Szamismetles_1 ~~ Szamismetles_2
+Szamsorozatvisszafele_1 ~~ Szamsorozatvisszafele_2
+
+Ujjak_1 ~~ Ujjak_2
+Kezek_1 ~~ Kezek_2
+
+Egyensuly_nyitottszem_jobb_1 ~~ Egyensuly_nyitottszem_jobb_2
+Egyensuly_nyitottszem_bal_1 ~~ Egyensuly_nyitottszem_bal_2
+
+Egyensuly_csukottszem_jobb_1 ~~ Egyensuly_csukottszem_jobb_2
+Egyensuly_csukottszem_bal_1 ~~ Egyensuly_csukottszem_bal_2
+
+'
+
+# ============================================================
+# 2. CONFIGURAL INVARIANCE
+# ============================================================
+
+fit_configural <- cfa(
+  model = long_model_configural,
+  data = Complete_cases_KPT2224,
+  std.lv = TRUE,
+  meanstructure = TRUE,
+  estimator = "MLR"
+)
+
+summary(
+  fit_configural,
+  standardized = TRUE,
+  fit.measures = TRUE
+)
+
+fitMeasures(
+  fit_configural,
+  c("chisq","df","cfi","tli","rmsea","srmr","aic","bic")
+)
+
+# ============================================================
+# 3. METRIC INVARIANCE
+# ============================================================
+
+long_model_metric <- '
+
+# ------------------------------------------------
+# TIME 1
+# ------------------------------------------------
+
+Gf_1 =~ a1*Raven_1 + a2*Figurak_1
+
+GWM_1 =~ b1*Corsi_1 +
+         b2*Szamismetles_1 +
+         b3*Szamsorozatvisszafele_1
+
+PSMF_1 =~ c1*Ujjak_1 + c2*Kezek_1
+
+BOF_1 =~ d1*Egyensuly_nyitottszem_jobb_1 +
+         d2*Egyensuly_nyitottszem_bal_1
+
+BCF_1 =~ e1*Egyensuly_csukottszem_jobb_1 +
+         e2*Egyensuly_csukottszem_bal_1
+
+
+# ------------------------------------------------
+# TIME 2
+# ------------------------------------------------
+
+Gf_2 =~ a1*Raven_2 + a2*Figurak_2
+
+GWM_2 =~ b1*Corsi_2 +
+         b2*Szamismetles_2 +
+         b3*Szamsorozatvisszafele_2
+
+PSMF_2 =~ c1*Ujjak_2 + c2*Kezek_2
+
+BOF_2 =~ d1*Egyensuly_nyitottszem_jobb_2 +
+         d2*Egyensuly_nyitottszem_bal_2
+
+BCF_2 =~ e1*Egyensuly_csukottszem_jobb_2 +
+         e2*Egyensuly_csukottszem_bal_2
+
+
+# ------------------------------------------------
+# LATENT STABILITY
+# ------------------------------------------------
+
+Gf_1 ~~ Gf_2
+GWM_1 ~~ GWM_2
+PSMF_1 ~~ PSMF_2
+BOF_1 ~~ BOF_2
+BCF_1 ~~ BCF_2
+
+
+# ------------------------------------------------
+# WITHIN-TIME FACTOR CORRELATIONS
+# ------------------------------------------------
+
+Gf_1 ~~ GWM_1 + PSMF_1 + BOF_1 + BCF_1
+GWM_1 ~~ PSMF_1 + BOF_1 + BCF_1
+PSMF_1 ~~ BOF_1 + BCF_1
+BOF_1 ~~ BCF_1
+
+Gf_2 ~~ GWM_2 + PSMF_2 + BOF_2 + BCF_2
+GWM_2 ~~ PSMF_2 + BOF_2 + BCF_2
+PSMF_2 ~~ BOF_2 + BCF_2
+BOF_2 ~~ BCF_2
+
+
+# ------------------------------------------------
+# CORRELATED UNIQUENESSES
+# ------------------------------------------------
+
+Raven_1 ~~ Raven_2
+Figurak_1 ~~ Figurak_2
+
+Corsi_1 ~~ Corsi_2
+Szamismetles_1 ~~ Szamismetles_2
+Szamsorozatvisszafele_1 ~~ Szamsorozatvisszafele_2
+
+Ujjak_1 ~~ Ujjak_2
+Kezek_1 ~~ Kezek_2
+
+Egyensuly_nyitottszem_jobb_1 ~~ Egyensuly_nyitottszem_jobb_2
+Egyensuly_nyitottszem_bal_1 ~~ Egyensuly_nyitottszem_bal_2
+
+Egyensuly_csukottszem_jobb_1 ~~ Egyensuly_csukottszem_jobb_2
+Egyensuly_csukottszem_bal_1 ~~ Egyensuly_csukottszem_bal_2
+
+'
+
+fit_metric <- cfa(
+  model = long_model_metric,
+  data = Complete_cases_KPT2224,
+  std.lv = TRUE,
+  meanstructure = TRUE,
+  estimator = "MLR"
+)
+
+fitMeasures(
+  fit_metric,
+  c("chisq","df","cfi","tli","rmsea","srmr","aic","bic")
+)
+
+# ============================================================
+# 4. MODEL COMPARISON
+# ============================================================
+
+anova(fit_configural, fit_metric)
+
+# ============================================================
+#
+# ============================================================
+
+
+
